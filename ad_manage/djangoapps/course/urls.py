e931f950bf63099fe478e521e95381d45b4d6f5d
santosa <angel_santos@outlook.com>
2017-04-25 12:35:03 -0500

    connection url create course

--- ad_manage/djangoapps/course/urls.py
+++ ad_manage/djangoapps/course/urls.py
@@ -2,8 +2,11 @@ from django.conf.urls import patterns, url
 
 from .views import (
                     CourseListIndexTemplateView,
+                    CourseCreateView,
                     )
 
 urlpatterns = [
     url(r'^$', CourseListIndexTemplateView.as_view(), name="course-list-index"),
+    url(r'^course/create/$', CourseCreateView.as_view(), name="course-create"),
+
 ]
